064d240781a895cccc0330018611f2dbc5d9891b
Wecks Salome Amadea <sw193@hdm-stuttgart.de>
2019-04-04 16:39:43 +0200

    neuer kommentar

--- src/main/java/AgChSaJo/JumpOrDie.java
+++ src/main/java/AgChSaJo/JumpOrDie.java
@@ -2,6 +2,7 @@ package AgChSaJo;
 
 public class JumpOrDie {
     public static void main(String[] args) {
-        System.out.println("Jump or Die!!");
+        System.out.println("Jump or Die!! !!!!");
+        System.out.println("Endlich funktioniert es");
     }
 }
