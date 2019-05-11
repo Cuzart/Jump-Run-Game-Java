9bd11e36b8fb3989455b24537c0f5f7d22a044de
Homar Franquesa Agil <ah210@hdm-stuttgart.de>
2019-05-11 13:39:33 +0200

    Scorelist idea

--- src/main/java/AgChSaJo/JumpOrDie/Player.java
+++ src/main/java/AgChSaJo/JumpOrDie/Player.java
@@ -8,6 +8,15 @@ public class Player implements IPlayer {
     private double finalScore, score;
     private double pHeight, pWidth, pY;
 
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public String getNickname(){
+        return nickname;
+    }
+    public double getFinalScore() {
+        return finalScore;
+    }
     public String[] getStats (){
         return new String[]{nickname, Double.toString(finalScore)};
     }
--- src/main/java/AgChSaJo/ScoreList/Scorelist.java
+++ src/main/java/AgChSaJo/ScoreList/Scorelist.java
@@ -1,16 +1,76 @@
 package AgChSaJo.ScoreList;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.List;
+import java.util.Scanner;
+
 public class Scorelist {
+    double finalScore = getFinalScore(double finalScore)
+    String finalScore = Double.toString(finalScore)
+    String nickname = getNickname(String nickname);
+    public void createEntry() { //writes the new score in the file ScoreList.txt if the finalScore is bigger than the ones in the file or the file has less than 20 scores
+        try {
+            int lines = CountLines();
+            if (lines < 20) {
+                FileWriter fw = new FileWriter("ScoreList.txt");
+                fw.write(nickname + " : " + finalScore);
+                fw.close();
+            } else if (finalScore > LowerScore()) {
 
-    void createEntry(){
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    private int CountLines(){ //count the lines in ScoreList.txt to see how many scores are saved on it
+        int numberOfLines = 0;
 
+        try {
+            BufferedReader br = new BufferedReader("ScoreList.txt");
+            while ((thisLine = br.readLine()) != null) {
+                numberOfLines ++;
+            }
+            return numberOfLines;
+        } catch(Exception e) {
+                e.printStackTrace();
+        }
+    }
+    public void ScoreLists(){ //convert the file into two lists one for the nicknames and one for the scores
+        String inputFileName = "ScoreList.txt";
+        File inputFile = new File(inputFileName);
+        List<String> nicknames= null;
+        List<Double> values=null;
+        try{
+            Scanner sc = new Scanner(inputFile);
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String [] parts = line.split(" : ");
+                nicknames.add(parts[0]);
+                values.add(Double.parseDouble(parts[1]));
+
+            }
+            return nicknames;
+            return values;
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    void getLowerScore() {
+
+        }
     }
 
-    void saveScoreToFile(){
+    /*void readFile(){
+        Scanner s = new Scanner(new File("ScoreList.txt"));
+        ArrayList<String> scores = new ArrayList<String>();
+        while (s.hasNext()){
+            scores.add(s.next());
+        }
+        s.close();
 
-    }
-
-    void readFile(){
-
-    }
+    } */
 }
