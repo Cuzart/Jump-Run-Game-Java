ba8cb01524ef0eaa929747ed515c4857351804fe
Homar Franquesa Agil <ah210@hdm-stuttgart.de>
2019-05-17 12:37:37 +0200

    Scorelist with JSON II

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module AgChSaJo {                     //package-Name
     requires javafx.controls;
     requires javafx.fxml;
     requires log4j.api;
+    requires json.simple;
     opens AgChSaJo to javafx.fxml, log4j.api;
     exports AgChSaJo;
 }
