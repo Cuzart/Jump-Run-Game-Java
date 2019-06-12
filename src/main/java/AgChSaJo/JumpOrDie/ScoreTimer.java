4f66593bb798e7c7d1545ec1ef2eface13156a57
Wecks Salome Amadea <sw193@hdm-stuttgart.de>
2019-06-12 15:49:59 +0200

    javadoc

--- src/main/java/AgChSaJo/JumpOrDie/ScoreTimer.java
+++ src/main/java/AgChSaJo/JumpOrDie/ScoreTimer.java
@@ -2,8 +2,14 @@ package AgChSaJo.JumpOrDie;
 
 import java.util.TimerTask;
 
+/**
+ * This class is a TimerTask for a Score Timer.
+ */
 public class ScoreTimer extends TimerTask {
 
+    /**
+     * This method adds one point to the score while the ScoreTimer is running.
+     */
     @Override
     public void run() {
         Board.addToScore(1);
